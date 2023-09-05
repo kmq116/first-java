61b1cdf605e3d3390a6c0e4c3bb1b4b93418222a
飞天御剑流co <521luqiya@gmail.com>
2023-09-05 11:00:26 +0800

    first api

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,14 +1,12 @@
 package org.example;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.context.annotation.ComponentScan;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 /**
  * @author coder
  */
-@EnableAutoConfiguration
-@ComponentScan
+@SpringBootApplication
 public class App {
     public static void main (String[] args) {
         SpringApplication.run(App.class,args);
--- src/main/java/org/example/controller/HelloController.java
+++ src/main/java/org/example/controller/HelloController.java
@@ -1,3 +1,5 @@
+package org.example.controller;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
