2c3ac0963fcd2a458950a4e93bfcb56496c2ae18
飞天御剑流co <521luqiya@gmail.com>
2023-09-07 13:31:12 +0800

    添加 book 增删查接口

--- src/main/java/org/example/controller/BookController.java
+++ src/main/java/org/example/controller/BookController.java
@@ -1,20 +1,31 @@
 package org.example.controller;
 
 import org.example.Book;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Date;
 
 @RestController
 public class BookController {
- @GetMapping("/book")
- public Book book(){
-     Book book = new Book();
-     book.setAuthor("罗贯中");
-     book.setName("三国演义");
-     book.setPrice(30f);
-     book.setPublicationDate(new Date());
-     return book;
- }
+    @GetMapping("/book")
+    public Book book() {
+        Book book = new Book();
+        book.setAuthor("罗贯中");
+        book.setName("三国演义");
+        book.setPrice(30f);
+        book.setPublicationDate(new Date());
+        return book;
+    }
+
+    @PostMapping("/book")
+    public String addBook(String name) {
+        return "receive post request" + name;
+    }
+
+    @DeleteMapping("/book/{id}")
+    public String deleteBook(@PathVariable Long id) {
+        return String.valueOf(id) + " deleted";
+    }
+
+
 }
